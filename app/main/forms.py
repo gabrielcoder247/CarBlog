f92d58ee034f230aae84a01f287529c012635c73
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-16 01:16:54 +0300

    create wtf forms for main form

---
+++ app/main/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import Required
+
+class BlogForm(FlaskForm):
+    title = StringField('Title', validators=[Required()],render_kw={"placeholder": "Title"})
+    blog_data = TextAreaField('Type Blog here', validators=[Required()])
+    photo_url = StringField('Photo URL', validators=[Required()], render_kw={'placeholder': 'Photo URL'})
+    post = SubmitField('Post Blog')
+
+class CommentForm(FlaskForm):
+    name = StringField('Name',validators=[Required()],render_kw={"placeholder": "Name"})
+    email = StringField('Email', validators=[Required()],render_kw={"placeholder": "Email"})
+    comment_data = TextAreaField('Enter Comment', validators=[Required()],render_kw={"placeholder": "Comment"})
+    post = SubmitField('Post Comment')
+
+class EmailForm(FlaskForm):
+    name = StringField('First Name', validators=[Required()],render_kw={"placeholder": "First Name"})
+    email = StringField('Email', validators=[Required()],render_kw={"placeholder": "Email"})
+    subscribe = SubmitField('Subscribe')

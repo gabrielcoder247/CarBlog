4148c9f9fe36e80f1ddacd2ecdcff87dd76f1d92
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-16 00:19:31 +0300

    create function for create app in manage.py

---
+++ manage.py
@@ -0,0 +1,31 @@
+from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
+from app.models import User,Pitch,Comment
+from app import create_app,db
+import unittest
+
+app = create_app('development')
+
+# Initializing extensions
+manager = Manager(app)
+manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)    
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db, User = User, Pitch = Pitch, Comment = Comment)
+
+@manager.command
+def test():
+    # Run the unit test   
+       
+    
+
+
+    tests= unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == '__main__':
+    manager.run()
+

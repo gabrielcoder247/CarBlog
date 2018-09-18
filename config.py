0f992d26a6394860ee4ae359c32e696efde648c5
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-18 14:55:17 +0300

    deploying to heroku

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://gabrielcoder:dushanbe2015@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    SECRET_KEY ='12345'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
